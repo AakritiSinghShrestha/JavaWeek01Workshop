54f596d4da8498f8c1306ab7b352aa19d292ef63
AakritiSinghShrestha <aakritibikash.stha88@gmail.com>
2020-10-28 03:10:28 +0000

    Completed  Core Java Hakim and Rasheeda

--- src/main/java/com/codedifferently/part01/Greetings.java
+++ src/main/java/com/codedifferently/part01/Greetings.java
@@ -1,7 +1,16 @@
 package com.codedifferently.part01;
 
+import java.util.Scanner;
+
 public class Greetings {
     public static void main(String[] args) {
-
+Scanner  scan = new Scanner(System.in);
+System.out.println("Enter your name");
+String name1 = scan.nextLine();
+String name2 = scan.nextLine();
+System.out.println("My name is " +name1 );
+System.out.println("Hi " +name1 );
+System.out.println("My name is " +name2 );
+System.out.println("Hi " +name2 );
     }
 }
